95053a8cde6a9d2d8e3045dfc0482d2ba4397409
Daniel Souza <Danielflorindo9@gmail.comgit config --global user.email Danielflorindo9@gmail.com>
2023-11-17 14:00:46 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import entities.model.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		System.out.println(new Department(1, "Books"));
+	}
+
+}

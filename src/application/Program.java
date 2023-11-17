b4dbe14b70202b76e472fbbeeb085287b426a28d
Daniel Souza <Danielflorindo9@gmail.comgit config --global user.email Danielflorindo9@gmail.com>
2023-11-17 14:17:32 -0300

    Classe vendedores criada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import entities.model.Department;
+import entities.model.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		System.out.println(new Department(1, "Books"));
+		System.out.println();
+		System.out.println(
+				new Seller(12, "Daniel Souza", "Daniel@gmail.com", new Date(), 2500.00, new Department(1, "Books")));
 	}
 
 }

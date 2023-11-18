f8d9ffbbcb671480342929efa692cc3e61cc6b80
Daniel Souza <Danielflorindo9@gmail.comgit config --global user.email Danielflorindo9@gmail.com>
2023-11-18 19:16:30 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,9 +9,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
+		
+		System.out.println("***** TESTE 01: Procurar o id de um vendedor *****");
 		Seller seller = sellerDao.findById(3);
-
 		System.out.println(seller);
 
 	}

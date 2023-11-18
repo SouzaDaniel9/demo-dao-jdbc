cfbe3dd756c7b16cd914980726377f9f71afe7fe
Daniel Souza <Danielflorindo9@gmail.comgit config --global user.email Danielflorindo9@gmail.com>
2023-11-18 19:04:56 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,5 @@
 package application;
 
-import java.util.Date;
-
-import entities.model.Department;
 import entities.model.Seller;
 import model.Dao.DaoFactory;
 import model.Dao.SellerDao;
@@ -11,13 +8,12 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println(new Department(1, "Books"));
-		System.out.println();
-		System.out.println(
-				new Seller(12, "Daniel Souza", "Daniel@gmail.com", new Date(), 2500.00, new Department(1, "Books")));
-	
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-	
+
+		Seller seller = sellerDao.findById(3);
+
+		System.out.println(seller);
+
 	}
 
 }

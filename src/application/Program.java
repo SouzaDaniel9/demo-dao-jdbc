c3853851587985503cc5d33debc2fae8fb3d9b37
Daniel Souza <Danielflorindo9@gmail.comgit config --global user.email Danielflorindo9@gmail.com>
2023-11-20 09:56:44 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.util.Date;
 import java.util.List;
 
 import entities.model.Department;
@@ -14,9 +13,9 @@ public class Program {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
-		System.out.println("***** TESTE 01: Procurar o id de um vendedor *****");
-		Seller seller = sellerDao.findById(3);
-		System.out.println(seller);
+//		System.out.println("***** TESTE 01: Procurar o id de um vendedor *****");
+//		Seller seller = sellerDao.findById(3);
+//		System.out.println(seller);
 
 		System.out.println("\n***** TESTE 02: Exibir os vendedores de um departamento *****");
 		Department department = new Department(2, null);
@@ -31,10 +30,16 @@ public class Program {
 			System.out.println(sel);
 		}
 
-		System.out.println("\n***** TESTE 04: Inserir Vendedor *****");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+//		System.out.println("\n***** TESTE 04: Inserir Vendedor *****");
+//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
+
+//		System.out.println("\n***** TESTE 05: Alterar dados de um vendedor *****");
+//		seller = sellerDao.findById(1);
+//		seller.setName("Martha Waine");
+//		sellerDao.update(seller);
+//		System.out.println("Update Completed");
 	}
 
 }
